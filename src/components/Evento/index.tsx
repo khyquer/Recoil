c7cac317aa1fce9c4ff67719e207c808e9f27e83
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2022-10-21 23:51:10 -0300

    Start

---
+++ src/components/Evento/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSetRecoilState } from 'recoil'
+import { IEvento } from '../../interfaces/IEvento'
+import { listaDeEventosState } from '../../state/atom'
+import style from './Evento.module.scss'
+import EventoCheckbox from './EventoCheckbox'
+
+const Evento: React.FC<{evento: IEvento}> = ({ evento }) => {
+	const estilos = [style.Evento]
+
+	if (evento.completo) {
+		estilos.push(style.completo)
+	}
+
+	const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
+	const aoDeletarEvento = () => {
+		setListaDeEventos((listaAntiga) =>
+			listaAntiga.filter((evt) => evt.id !== evento.id)
+		)
+	}
+
+	return (
+		<div className={estilos.join(' ')}>
+			<EventoCheckbox evento={evento} />
+			<div className='cards-info'>
+				<h3 className={style.descricao}>
+					{evento.descricao} - {evento.inicio.toLocaleDateString()}
+				</h3>
+			</div>
+			<i
+				className='far fa-times-circle fa-2x'
+				onClick={aoDeletarEvento}></i>
+		</div>
+	)
+}
+
+export default Evento
+
---
+++ src/state/atom.ts
@@ -0,0 +1,29 @@
+import { atom } from 'recoil'
+import { IEvento } from '../interfaces/IEvento'
+
+export const listaDeEventosState = atom<IEvento[]>({
+	key: 'listaDeEventosState',
+	default: [
+		{
+			descricao: 'Estudar React',
+			inicio: new Date('2022-01-15T09:00'),
+			fim: new Date('2022-01-15T13:00'),
+			completo: false,
+			id: 1642342747,
+		},
+		{
+			descricao: 'Estudar Recoil',
+			inicio: new Date('2022-01-16T09:00'),
+			fim: new Date('2022-01-16T11:00'),
+			completo: false,
+			id: 1642342959,
+		},
+		{
+			descricao: 'Estudar Recoil 2',
+			inicio: new Date('2022-10-21T09:00'),
+			fim: new Date('2022-10-21T11:00'),
+			completo: false,
+			id: 1642342958,
+		}
+	],
+})
